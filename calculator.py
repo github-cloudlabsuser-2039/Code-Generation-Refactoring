a4e425fbdd93e5afdba7cdd416a33d5167a98e6c
Damian <github_cloudlabsuser_2039@cloudlabsaiuser.com>
2025-03-26 14:29:53 +0000

    calculator

---
+++ calculator.py
@@ -0,0 +1,20 @@
+
+class Calculadora:
+  ///funcion que devuelve la suma de dos numeros
+      def suma(self, a, b):
+        # Implementación vacía
+        pass
+
+    def resta(self, a, b):
+        # Implementación vacía
+        pass
+
+    def multiplicacion(self, a, b):
+        # Implementación vacía
+        pass
+
+    def division(self, a, b):
+        if b == 0:
+            raise ValueError("No se puede dividir por 0")
+        # Implementación vacía
+        pass
